200e52897bdc1fe904278caafeada6847312209a
MuhammadArtaza5290 <hafizartaza098@gmail.com>
2025-07-03 10:30:51 -0700

    3-7-2025 make cart in this commit

--- node prac/SCATCH_Porject/routes/index.js
+++ node prac/SCATCH_Porject/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('../middlewares/isLoggedIn');
 const productModel = require('../models/product-model')
+const userModel = require('../models/user-model')
 router.get('/', function(req, res){
     let error = req.flash('error')
     res.render('index', {error})
@@ -9,8 +10,22 @@ router.get('/', function(req, res){
 
 router.get('/shop',isLoggedIn, async function(req, res){
     let products = await productModel.find()
-    res.render('shop' , { products })
+    let success = req.flash('success')
+    res.render('shop' , { products, success })
 })
 
+router.get('/cart',isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({email: req.user.email}).populate('cart')
+    console.log(user);
+    
+    res.render('cart')
+})
 
+router.get('/addtocart/:productid', isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({email: req.user.email});
+    user.cart.push(req.params.productid)
+    await user.save()
+    req.flash('success', 'Added to cart')
+    res.redirect('/shop')
+})
 module.exports = router
